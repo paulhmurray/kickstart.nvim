7e44ed06d91cef36b6138b9502d804efa7570974
Paul Murray <paulthemurray@gmail.com>
2024-06-17 22:56:45 +1000

    noice and auto complete noice

--- init.lua
+++ init.lua
@@ -317,6 +317,52 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'goolord/alpha-nvim',
+    config = function()
+      local alpha = require 'alpha'
+      local dashboard = require 'alpha.themes.dashboard'
+      dashboard.section.header.val = {
+        [[        ___                     ___          _____         ]],
+        [[       /  /\      ___          /__/\        /  /::\        ]],
+        [[      /  /:/_    /  /\         \  \:\      /  /:/\:\       ]],
+        [[     /  /:/ /\  /  /:/          \  \:\    /  /:/  \:\      ]],
+        [[    /  /:/ /:/ /__/::\      _____\__\:\  /__/:/ \__\:|     ]],
+        [[   /__/:/ /:/  \__\/\:\__  /__/::::::::\ \  \:\ /  /:/     ]],
+        [[   \  \:\/:/      \  \:\/\ \  \:\~~\~~\/  \  \:\  /:/      ]],
+        [[    \  \::/        \__\::/  \  \:\  ~~~    \  \:\/:/       ]],
+        [[     \  \:\        /__/:/    \  \:\         \  \::/        ]],
+        [[      \  \:\       \__\/      \  \:\         \__\/         ]],
+        [[       \__\/                   \__\/                       ]],
+        [[        ___                       ___           ___        ]],
+        [[       /  /\                     /  /\         /__/\       ]],
+        [[      /  /:/_                   /  /::\       _\_ \:\      ]],
+        [[     /  /:/ /\  ___     ___    /  /:/\:\     /__/\ \:\     ]],
+        [[    /  /:/ /:/ /__/\   /  /\  /  /:/  \:\   _\_ \:\ \:\    ]],
+        [[   /__/:/ /:/  \  \:\ /  /:/ /__/:/ \__\:\ /__/\ \:\ \:\   ]],
+        [[   \  \:\/:/    \  \:\  /:/  \  \:\ /  /:/ \  \:\ \:\/:/   ]],
+        [[    \  \::/      \  \:\/:/    \  \:\  /:/   \  \:\ \::/    ]],
+        [[     \  \:\       \  \::/      \  \:\/:/     \  \:\/:/     ]],
+        [[      \  \:\       \__\/        \  \::/       \  \::/      ]],
+        [[       \__\/                     \__\/         \__\/       ]],
+      }
+      dashboard.section.buttons.val = {
+        dashboard.button('e', '  New file', '<cmd>ene <CR>'),
+        dashboard.button('SPC s .', '󰈞  Recently opened files'),
+        dashboard.button('q', '󰅚  Quit NVIM', ':qa<CR>'),
+      }
+      local handle = io.popen 'fortune'
+      local fortune = handle:read '*a'
+      handle:close()
+      dashboard.section.footer.val = fortune
+
+      dashboard.config.opts.noautocmd = true
+
+      vim.cmd [[autocmd User AlphaReady echo 'ready']]
+
+      alpha.setup(dashboard.config)
+    end,
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
@@ -324,7 +370,6 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -740,6 +785,29 @@ require('lazy').setup({
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
+
+      -- `/` cmdline setup.
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' },
+        },
+      })
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' },
+        }, {
+          {
+            name = 'cmdline',
+            option = {
+              ignore_cmds = { 'Man', '!' },
+            },
+          },
+        }),
+      })
 
       cmp.setup {
         snippet = {
